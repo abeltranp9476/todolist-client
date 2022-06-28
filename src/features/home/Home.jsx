d95604aa2e36edd825acf567b1b909b3bc2c2d0a
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-06-28 16:42:49 -0400

    feat: show Todolist component

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,18 +1,32 @@
+import useEnhancedEffect from '@mui/utils/useEnhancedEffect';
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectLogin } from '../login/loginSlice';
-
-
+import Todolist from '../todolist/Todolist';
+import { fetchtodos } from '../todolist/todolistAPI'
 
 function Home() {
-    const session = useSelector(selectLogin);
-    const dispatch = useDispatch();
+    const [todos, setTodos] = useState([])
 
+    const loadInitData = async () => {
+        const todoList = await fetchtodos()
+        setTodos(todoList);
+    }
+
+    useEffect(() => {
+        let mounted = true;
+
+        if (mounted) {
+            loadInitData()
+        }
+
+        return () => {
+            mounted = false;
+        };
+    }, [])
 
     return (
-        <>
-            <h1>Bienvenido</h1>
-        </>
+        <Todolist todos={todos?.data?.data} />
     )
 }
 
