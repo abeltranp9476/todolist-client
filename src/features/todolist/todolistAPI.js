8412dcdff7a481aa94eff590f1d9e76906c0faff
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-07-04 16:15:56 -0400

    Update todolistAPI.js

--- src/features/todolist/todolistAPI.js
+++ src/features/todolist/todolistAPI.js
@@ -1,7 +1,7 @@
 import { api } from '../../utils/api';
 import { tokenUtils } from '../../utils/authentication';
 
-export const fetchtodos = async () => {
+export const fetchTodos = async () => {
     const data = await tokenUtils.getAuthenticationToken();
     let token = null;
     if (data) {
@@ -14,4 +14,76 @@ export const fetchtodos = async () => {
         }
     });
 
+}
+
+export const fetchTodo = async (id) => {
+    const data = await tokenUtils.getAuthenticationToken();
+    let token = null;
+    if (data) {
+        token = data.split(':')[1];
+    }
+
+    return await api.get(`todo/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+}
+
+export const createTodo = async (name, description) => {
+    const form = new FormData();
+    form.append('name', name);
+    form.append('description', description);
+    form.append('device', 'react');
+
+    const data = await tokenUtils.getAuthenticationToken();
+    let token = null;
+    if (data) {
+        token = data.split(':')[1];
+    }
+
+    return await api.post('todo', form, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
+
+export const updateTodo = async (id, name, description) => {
+    const form = new FormData()
+    form.append('name', name)
+    form.append('description', description)
+    form.append('device', 'react')
+    form.append('_method', 'PUT')
+
+    const data = await tokenUtils.getAuthenticationToken()
+    let token = null
+    if (data) {
+        token = data.split(':')[1]
+    }
+
+    return await api.post(`todo/${id}`, form, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
+
+export const deleteTodo = async (id) => {
+    const form = new FormData()
+    form.append('device', 'react')
+    form.append('_method', 'DELETE')
+
+    const data = await tokenUtils.getAuthenticationToken()
+    let token = null
+    if (data) {
+        token = data.split(':')[1]
+    }
+
+    return await api.post(`todo/${id}`, form, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
 }
